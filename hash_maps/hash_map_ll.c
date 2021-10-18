bc2ebeffe71f84b392aca691b2f197d69f35f3ca
Alex Dillhoff <ajdillhoff@gmail.com>
2021-10-18 10:58:48 -0500

    Updated example

--- hash_maps/hash_map_ll.c
+++ hash_maps/hash_map_ll.c
@@ -5,40 +5,47 @@
 #define MAP_SIZE 10
 #define BUF_SIZE 128
 
-typedef struct node Node;
-struct node {
+typedef struct {
     char *key;
     char *value;
-    Node *next;
+} hash_element_t;
+
+typedef struct node node_t;
+struct node {
+    hash_element_t *data;
+    node_t *next;
 };
 
 int hash_function(char *key) {
     printf("key: %s\n", key);
 
-    int sum = 0;
+    int hash = 0;
     for (int i = 0; i < strlen(key); i++) {
-        sum += key[i];
+        hash += key[i];
     }
 
-    printf("hash: %d\n", sum);
+    printf("hash: %d\n", hash);
 
-    return sum;
+    return hash;
 }
 
 int compute_index(char *key, int array_size) {
     int hash = hash_function(key);
     int index = hash % array_size;
 
-    printf("index = %d\n", index);
+    printf("index: %d\n", index);
 
     return index;
 }
 
-void add_node(Node **head, Node *new_node) {
+void add_node(node_t **head, hash_element_t *data) {
+    node_t *new_node = calloc(1, sizeof(node_t));
+    new_node->data = data;
+
     if (*head == NULL) {
         *head = new_node;
     } else {
-        Node *temp = *head;
+        node_t *temp = *head;
 
         while (temp->next != NULL) {
             temp = temp->next;
@@ -48,49 +55,64 @@ void add_node(Node **head, Node *new_node) {
     }
 }
 
-void traverse(Node *n) {
-    if (n != NULL) {
-        printf("(%s %s)\n", n->key, n->value);
-        traverse(n->next);
+void traverse(node_t *n) {
+
+    if (n == NULL) {
+        printf("EMPTY");
+    }
+
+    while (n != NULL) {
+        printf("(\"%s\", \"%s\") ", n->data->key, n->data->value);
+
+        n = n->next;
+
+        if (n != NULL) {
+            printf("-> ");
+        }
+    }
+    printf("\n");
+}
+
+void print_map(node_t *map[], int array_size) {
+    for (int i = 0; i < array_size; i++) {
+        printf("map[%d] = ", i);
+        traverse(map[i]);
     }
 }
 
 int main() {
     char buffer[BUF_SIZE] = { 0 };
-    Node *hash_map[MAP_SIZE] = { NULL };
+    node_t *map[MAP_SIZE] = { NULL };
 
     while (1) {
-        Node *new_node = calloc(1, sizeof(Node));
-
-        // get key from user
-        printf("Enter key: ");
+        printf("Enter a key: ");
         fgets(buffer, BUF_SIZE, stdin);
         buffer[strlen(buffer) - 1] = 0;
 
-        // copy key data to node
-        new_node->key = malloc(sizeof(char) * (strlen(buffer) + 1));
-        strcpy(new_node->key, buffer);
+        // Copy the data to a hash_element_t
+        hash_element_t *elem = calloc(1, sizeof(hash_element_t));
+        elem->key = malloc(sizeof(char) * (strlen(buffer) + 1));
+        strcpy(elem->key, buffer);
 
-        // convert key to an index
-        int index = compute_index(new_node->key, MAP_SIZE);
+        // Convert the key to an index
+        int index = compute_index(elem->key, MAP_SIZE);
 
-        // check for collision
-        if (hash_map[index] != NULL) {
-            printf("COLLISION DETECTED!\n");
+        // Check for collisions
+        if (map[index] != NULL) {
+            printf("COLLISION DETECTED\n");
         }
 
-        // get value from user
-        printf("Enter value: ");
+        // Get value from the user
+        printf("Enter a value: ");
         fgets(buffer, BUF_SIZE, stdin);
         buffer[strlen(buffer) - 1] = 0;
 
-        // copy value data to node
-        new_node->value = malloc(sizeof(char) * (strlen(buffer) + 1));
-        strcpy(new_node->value, buffer);
+        elem->value = malloc(sizeof(char) * (strlen(buffer) + 1));
+        strcpy(elem->value, buffer);
 
-        // Add node to the index
-        add_node(&hash_map[index], new_node);
-        traverse(hash_map[index]);
+        // Add element to the map
+        add_node(&map[index], elem);
+        print_map(map, MAP_SIZE);
     }
 
     return 0;
