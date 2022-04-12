502c6afe1ad4bed3c4df496e849dec8fc96cd45b
Alex Dillhoff <ajdillhoff@gmail.com>
2022-04-12 11:11:44 -0500

    Added makefile example

---
+++ make/src/hash_map_utils.h
@@ -0,0 +1,32 @@
+#ifndef HASH_MAP_UTILS_H_
+#define HASH_MAP_UTILS_H_
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define LOAD_FACTOR 0.75
+#define R_VALUE 1
+
+typedef struct {
+    char *key;
+    void *value;
+} hash_element_t;
+
+typedef struct {
+    hash_element_t **temp;
+    hash_element_t **primary;
+    int num_keys;
+    int temp_size;
+    int map_size;
+    int temp_index;
+} hash_map_t;
+
+int hash_function(char *);
+int compute_index(char *, int);
+void init_map(hash_map_t *, int);
+hash_element_t *search(hash_map_t *, char *);
+void rehash_inc(hash_map_t *);
+void insert(hash_map_t *, hash_element_t *);
+
+#endif
