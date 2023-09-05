16069ff3213f11f331810b8a80adb4408d38fcef
Alex Dillhoff <ajdillhoff@gmail.com>
2023-09-05 16:33:00 -0500

    Added comments to example.

--- file_io/read_csv_file.c
+++ file_io/read_csv_file.c
@@ -3,20 +3,28 @@
 
 #define BUFFER_SIZE 1024
 
+// Removes the newline character from the string
+// Supports both Windows and UNIX style newlines
 void trim(char *str) {
-    int l = strlen(str);
-    if (str[l-1] == '\n') {
-        str[l-1] = 0;
-    }
+    str[strcspn(str, "\r\n")] = 0;
 }
 
+// Parses a line of CSV data and prints the tokens.
+// Returns the number of tokens read.
 int parse_line(char *buffer) {
     int result = 0;
+
+    // Return the first token (this will replace the first comma with a null character)
     char *token = strtok(buffer, ",");
 
+    // Loop through the string while there are still tokens to process
     while (token != NULL) {
+        // Since a token was read, increment the result
         result++;
         printf("%-15s ", token);
+
+        // Attempt to read the next token.
+        // This will return NULL if there are no more tokens.
         token = strtok(NULL, ",");
     }
 
@@ -29,6 +37,13 @@ int main() {
     char buffer[BUFFER_SIZE] = { 0 };
     FILE *fp = fopen("prices.csv", "r");
 
+    // Ensure the file was opened.
+    if (fp == NULL) {
+        printf("Failed to open file.\n");
+        return 1;
+    }
+
+    // Keep reading each line while there is still data to be read.
     while (fgets(buffer, BUFFER_SIZE, fp)) {
         trim(buffer);
         int num_tokens = parse_line(buffer);
